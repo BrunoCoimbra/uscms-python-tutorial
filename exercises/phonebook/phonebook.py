19d8163042ec83fe580c2af2f19dbf47bbaa94da
Bruno Coimbra <coimbra@live.com>
2024-05-24 12:37:45 -0500

    first commit

---
+++ exercises/phonebook/phonebook.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""Phone book exercise"""
+
+# Write a program that implements a simple phone book.
+#
+# The program should prompt the user for a name and return
+# the corresponding contact information.
+#
+# The user can type index to get a list of all names in the
+# phone book, and can type quit to exit the program.
+#
+# The phone book should be stored in a file called phonebook.txt
+# (provided in the exercise folder)
+
+# Learning objectives:
+#   - input and output
+#   - functions
+#   - if statements
+#   - read files
+#   - for and while loops
+#   - dictionaries
+#   - error handling
+#   - list comprehensions
